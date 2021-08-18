4c46d570d35de3dec7cb54b25ddd7b54abc47f27
RYOTAN9688 <ryota8813@gmail.com>
2021-08-18 20:49:30 +0900

    bootstrap err

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,6 @@
+//yarnでインストールしたbootstrapのパッケージを利用できるようにする
+import 'bootstrap'
+import '../stylessheets/application'
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
