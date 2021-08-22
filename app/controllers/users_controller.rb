b46c4d00c8c26ebc4f155a0822437ea6e7d31081
RYOTAN9688 <ryota8813@gmail.com>
2021-08-22 14:10:20 +0900

    ルーティング完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,13 @@ class UsersController < ApplicationController
     #params 送られてきたリクエストをひとまとめにしてparms[:パラメータ名]という形式で取得できる
       @user = User.find(params[:id])
   end
+
+  def index
+    #whereは条件にマッチするレコードを全て返す
+    #where.notは条件にマッチしないレコードをすべて返す
+    #現在ログインしているユーザ（自分）以外のユーザーのレコードを取得（スワイプページで自分を表示させないため）
+    @users = User.where.not(id: current_user.id)
+    #@userは現在ログインしているユーザー（自分）のレコード情報が入る
+    @user = User.find(current_user.id)
+  end
 end
